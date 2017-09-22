89160d59da80ede77c7676df8072b69530247a59
Dennis <dennisboer@gmail.com>
2017-09-22 10:43:21 +0200

    eerste functie aangemaakt

--- Battlebot/Battlebot.ino
+++ Battlebot/Battlebot.ino
@@ -13,12 +13,55 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
+int in1 = 9;
+int in2 = 4;
+int in3 = 3;
+int in4 = 2;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
 
+void stopEngine()
+{
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void forwardEngineLeft(int speed)
+{
+  analogWrite(in3, speed);
+  digitalWrite(in4, LOW);
+}
+
+void forwardEngineRight(int speed)
+{
+  analogWrite(in1, speed);
+  digitalWrite(in2, LOW);
+}
+
+void turnRight(int speed)
+{
+  analogWrite(in1, speed);
+  digitalWrite(in2, HIGH);
+  analogWrite(in3, speed);
+  digitalWrite(in4, LOW);
+}
+
+void turnLeft(int speed)
+{
+  analogWrite(in1, speed);
+  digitalWrite(in2, LOW);
+  analogWrite(in3, speed);
+  digitalWrite(in4, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
