ee99a2ac66f142d9c33bacb45a0fef81d3730e66
Dennis <dennisboer@gmail.com>
2017-09-22 10:48:22 +0200

    functies in apart bestand

--- Battlebot/Battlebot.ino
+++ Battlebot/Battlebot.ino
@@ -26,42 +26,8 @@ void setup() {
   pinMode(in4, OUTPUT);
 }
 
-void stopEngine()
-{
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-}
 
-void forwardEngineLeft(int speed)
-{
-  analogWrite(in3, speed);
-  digitalWrite(in4, LOW);
-}
-
-void forwardEngineRight(int speed)
-{
-  analogWrite(in1, speed);
-  digitalWrite(in2, LOW);
-}
-
-void turnRight(int speed)
-{
-  analogWrite(in1, speed);
-  digitalWrite(in2, HIGH);
-  analogWrite(in3, speed);
-  digitalWrite(in4, LOW);
-}
-
-void turnLeft(int speed)
-{
-  analogWrite(in1, speed);
-  digitalWrite(in2, LOW);
-  analogWrite(in3, speed);
-  digitalWrite(in4, HIGH);
-}
 
 void loop() {
-  
+  turnRight(100);
 }
